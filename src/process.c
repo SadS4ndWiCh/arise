e20540181d612aed4a43d9b42473f5a3404e74e1
sads4ndwich <caiocamargo007@gmail.com>
2024-10-30 10:49:35 -0300

    feat: reap dead processes

---
+++ src/process.c
@@ -0,0 +1,28 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#include "process.h"
+
+static void sigchld_handler(int s) {
+    int saved_errno = errno;
+
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+void reap_processes(void) {
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+
+}
